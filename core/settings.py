bcf60d4905ecc31e675e33a1ace640d2d34aab73
EnockOMONDI <enockomondike@gmail.com>
2020-10-06 04:22:07 +0300

    changed Indentation in SETTINGS

--- core/settings.py
+++ core/settings.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 if DEBUG:
-DATABASES = {
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
@@ -79,7 +79,7 @@ DATABASES = {
 }
 
 else: 
-DATABASES = {
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'jihusisheDB00',
