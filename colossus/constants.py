b98d9715c6293dedef4f264f02723371d51df43d
EnockOMONDI <enockomondike@gmail.com>
2020-10-09 12:47:24 +0300

    changed IP

---
+++ colossus/constants.py
@@ -0,0 +1,22 @@
+SB_ADMIN_CLIENT_CSS_LIBRARY_URLS = [ 
+                            "assets2/css/app.min.css",
+                            "assets2/css/style.css",
+                            "assets2/css/components.css",
+                            "assets2/bundles/bootstrap-social/bootstrap-social.css",
+                            "assets2/bundles/flag-icon-css/css/flag-icon.min.css",
+                            "assets2/bundles/owlcarousel2/dist/assets/owl.carousel.min.css",
+                            "assets2/bundles/owlcarousel2/dist/assets/owl.theme.default.min.css",
+                             "assets2/bundles/summernote/summernote-bs4.css",
+
+]
+
+SB_ADMIN_CLIENT_JS_LIBRARY_URLS = [  
+                            "assets2/js/app.min.js",
+                            "assets2/bundles/echart/echarts.js",  
+                            "assets2/bundles/chartjs/chart.min.js",
+                            "assets2/bundles/apexcharts/apexcharts.min.js",
+                            "assets2/js/page/index.js",
+                            "assets2/js/scripts.js",
+                            "assets2/bundles/jquery.sparkline.min.js"
+  
+]
